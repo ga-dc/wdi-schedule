a22416f2160a3e591c7bbe0055b3e2a4a936729d
Nick Olds <nolds9@yahoo.com>
2016-06-10 14:40:45 -0400

    parses yaml

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
 require 'yaml'
-thing = YAML.load_file('schedule.yml')
-thing.each do |t|
-p t
-p "===="
+require 'pry'
+days = YAML.load_file('schedule.yml')
+days.each_with_index do |day, index|
+  puts "# Day #{index + 1}"
+  day["day"].each do |events|
+    events.each do |time, details|
+      puts "## #{time}"
+      puts "### #{details["title"]}"
+      puts "### #{details["url"]}"
+      puts "### #{details["lead"]}"
+      puts "### #{details["support"]}"
+    end
+  end
 end
