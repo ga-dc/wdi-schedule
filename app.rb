a34996e6ef3b504bdaadb7691cf53ce02b3d04b5
Jesse Shawl <jesse@jshawl.com>
2016-06-10 14:51:54 -0400

    generate html

--- app.rb
+++ app.rb
@@ -1,15 +1,27 @@
 require 'yaml'
 require 'pry'
+
 days = YAML.load_file('schedule.yml')
 days.each_with_index do |day, index|
-  puts "# Day #{index + 1}"
+  puts "<div class='day'>"
+  puts "<h2> Day #{index + 1}</h2>"
   day["day"].each do |events|
     events.each do |time, details|
-      puts "## #{time}"
-      puts "### #{details["title"]}"
-      puts "### #{details["url"]}"
-      puts "### #{details["lead"]}"
-      puts "### #{details["support"]}"
+      title = details["title"]
+      url = details["url"]
+      lead = details["lead"]
+      support = details["support"]
+      puts "
+      <div class='event'>
+	<h2><a href='#{url}'>#{time}: #{title}</a></h2>
+	<ul>
+	  <li>#{lead}</li>
+	  <li>#{support}</li>
+	</ul>
+      </div>"
     end
   end
+  puts "</div>"
 end
+
+
