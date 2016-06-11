fac8e375c0a40a7592dc85f7a8bf6ddad4544f44
Nick Olds <nolds9@yahoo.com>
2016-06-10 23:34:10 -0400

    wip: binge code parses event title, lead, url, and support

--- ical.rb
+++ ical.rb
@@ -1,6 +1,6 @@
 require 'pry'
 events = []
-real_events = []
+wdi_events = []
 temp = {}
 
 def strftime str
@@ -25,15 +25,44 @@ end
 
 events.each do |event|
   if event["BEGIN"] == "VEVENT" && event["DTSTART"]
-    real_events << event
+    wdi_events << event
   end
 end
 
-real_events.each do |event|
+# given an event's summary as a string
+# returns array with event's title, lead, and support
+# NOTE assumes most events are following naming convention: "(Adrian / Jesse) Git Branching"
+def parse_summary str
+  info = []
+  if str.include?("/") # lesson
+    opi = str.index(/\(/)
+    cpi = str.index(/\)/)
+    fsi = str.index(/\//) # convention: lead before slash, support after
+    info << str[cpi + 2, str.length - 1] # title
+    info << str[opi + 1, fsi - 1] # lead
+    info << str[fsi + 1..cpi-1] # support
+  elsif str.include?(")") # no support scheduled
+    # TODO deal with non-conventional parens in event title
+    info << str[str.index(/\)/) +2, str.length - 1]
+    info << str[/\((.*?)\)/, 1] # grab anything between parens
+    info << ""
+  else # no support or lead
+    info = [str, "", ""]
+  end
+  info
+end
+
+wdi_events.each do |event|
   start = strftime(event["DTSTART"])
   endd = strftime(event["DTEND"])
+  info = parse_summary(event["SUMMARY"])
+  url = event["DESCRIPTION"][/\"(.*?)\"/, 1] if event["DESCRIPTION"].include?("\<a") # TODO deal with multiple urls
   puts "- day:"
   puts " - #{start} - #{endd}:"
+  puts "   title: #{info[0]}"
+  puts "   url: #{url}"
+  puts "   lead: #{info[1]}"
+  puts "   support: #{info[2]}"
 end
 
 
